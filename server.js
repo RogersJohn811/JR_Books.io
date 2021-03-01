f199e5041dfc01469ca0d13f4f45da206d908a22
RogersJohn811 <rogersjohn811@gmail.com>
2021-02-28 20:14:41 -0500

    Fixing server issues

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+// require("dotenv").config({path: './config.env'})
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,9 +12,22 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://HishamKader:<butter47>@cluster0.9cdum.mongodb.net/<googlebooks>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  
+  client.close();
+});
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+})
 
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
