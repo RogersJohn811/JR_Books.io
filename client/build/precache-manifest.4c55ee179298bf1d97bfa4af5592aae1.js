61ded86f329087eb6c4b99a40135dff5007b7f4e
RogersJohn811 <rogersjohn811@gmail.com>
2021-02-28 22:18:17 -0500

    Fixing build

---
+++ client/build/precache-manifest.4c55ee179298bf1d97bfa4af5592aae1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08510a8e4087990992533202acca0ac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d8e44c915f1e13eab37",
+    "url": "/static/css/2.7ca58fb4.chunk.css"
+  },
+  {
+    "revision": "b6468d8107fe9645dbfe",
+    "url": "/static/css/main.3f860968.chunk.css"
+  },
+  {
+    "revision": "8d8e44c915f1e13eab37",
+    "url": "/static/js/2.99a7b324.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/static/js/2.99a7b324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6468d8107fe9645dbfe",
+    "url": "/static/js/main.a1b1b8dd.chunk.js"
+  },
+  {
+    "revision": "53baf01953e240e32809",
+    "url": "/static/js/runtime-main.548fa050.js"
+  }
+]);
